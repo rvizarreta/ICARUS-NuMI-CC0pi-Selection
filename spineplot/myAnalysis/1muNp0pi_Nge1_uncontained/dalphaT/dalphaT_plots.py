b9364327c28450d21baaac87f992cf25dfc4dd98
Renzo Vizarreta <rvizarreta14@gmail.com>
2026-06-30 21:23:17 -0500

    comment stuff

--- spineplot/myAnalysis/1muNp0pi_Nge1_uncontained/dalphaT/dalphaT_plots.py
+++ spineplot/myAnalysis/1muNp0pi_Nge1_uncontained/dalphaT/dalphaT_plots.py
@@ -12,11 +12,11 @@ ana_directory = os.path.dirname(os.path.abspath(__file__))
 
 def run(close_figs=True):
     Analysis(os.path.join(ana_directory, 'dalphaT.toml'), data_file).run(close_figs=close_figs)
-    Analysis(os.path.join(ana_directory, 'dalphaT_xSecUncertainty.toml'), data_file).run(close_figs=close_figs)
-    Analysis(os.path.join(ana_directory, 'dalphaT_xSecUncertainty_Exp.toml'), data_file).run(close_figs=close_figs)
-    Analysis(os.path.join(ana_directory, 'dalphaT_FluxUncertainty.toml'), data_file).run(close_figs=close_figs)
-    Analysis(os.path.join(ana_directory, 'dalphaT_FluxUncertainty_Exp.toml'), data_file).run(close_figs=close_figs)
-    Analysis(os.path.join(ana_directory, 'dalphaT_DetectorUncertainty.toml'), data_file).run(close_figs=close_figs)
+    # Analysis(os.path.join(ana_directory, 'dalphaT_xSecUncertainty.toml'), data_file).run(close_figs=close_figs)
+    # Analysis(os.path.join(ana_directory, 'dalphaT_xSecUncertainty_Exp.toml'), data_file).run(close_figs=close_figs)
+    # Analysis(os.path.join(ana_directory, 'dalphaT_FluxUncertainty.toml'), data_file).run(close_figs=close_figs)
+    # Analysis(os.path.join(ana_directory, 'dalphaT_FluxUncertainty_Exp.toml'), data_file).run(close_figs=close_figs)
+    # Analysis(os.path.join(ana_directory, 'dalphaT_DetectorUncertainty.toml'), data_file).run(close_figs=close_figs)
 
 if __name__ == '__main__':
     run()
